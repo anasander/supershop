d46e3192054e281a4d16a141995a5ba18e2aa4a2
Ana Sander <anagsander@gmail.com>
2026-08-11 18:08:43 -0300

    feat: :sparkles: new mysql implementation

--- supershop.py
+++ supershop.py
@@ -1,19 +1,46 @@
-from flask import Flask, render_template, request, make_response
+from flask import Flask, render_template, request, url_for, redirect, make_response
 from markupsafe import escape
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@127.0.0.1:3306/supershop'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://supershop:1234567890@localhost:3306/supershop'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Usuario(db.Model):
+    id = db.Column('usuario_id', db.Integer, primary_key=True)
+    name = db.Column('usuario_nome', db.String(100))
+    email = db.Column('usuario_email', db.String(100))
+    password = db.Column('usuario_senha', db.String(100))
+    address = db.Column('usuario_endereco', db.String(200))
+
+    def __init__(self, name, email, password, address):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.address = address
 
 @app.route("/")
 def index():
     return render_template("index.html", titulo="Página Inicial")
 
 @app.route("/cadastro/usuario")
-def cadastroUsuario():
-    return render_template("usuario.html", titulo="Cadastro de usuário")
+def usuario():
+    return render_template("usuario.html", usuarios = Usuario.query.all(), titulo="Usuário")
 
 @app.route("/cadastro/caduser", methods=["POST"])
 def caduser():
-    return request.form
+    usuario = Usuario(
+        request.form.get("name"),
+        request.form.get("email"),
+        request.form.get("password"),
+        request.form.get("address")
+    )
+    db.session.add(usuario)
+    db.session.commit()
+    return redirect(url_for('usuario'))
 
 @app.route("/cadastro/produto")
 def cadastroProduto():
@@ -49,3 +76,7 @@ def relatorioVendas():
 @app.route("/relatorios/compras")
 def relatorioCompras():
     return render_template("relatorioCompras.html", titulo="Relatório de Compras")
+
+if __name__ == "supershop":
+    with app.app_context():
+        db.create_all()
